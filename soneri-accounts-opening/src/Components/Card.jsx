7b5e38dc5910f052d22b9968e322c52c623def13
Naif Ibrahim <naifibrahim125@gmail.com>
2024-07-05 18:18:24 +0500

    WIP

--- soneri-accounts-opening/src/Components/Card.jsx
+++ soneri-accounts-opening/src/Components/Card.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
-import { Grid, Card, CardContent, Typography, CardActions, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Grid, Card, CardContent, Typography, CardActions, Button, Modal, Box } from '@mui/material';
+
+const style = {
+  display: 'flex',
+  flexDirection: 'column',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 900,
+  borderRadius: '5px',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 2,
+};
 
 const GenericCard = ({ title, description, primaryActionText, secondaryActionText, onPrimaryActionClick, onSecondaryActionClick }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
@@ -14,14 +32,76 @@ const GenericCard = ({ title, description, primaryActionText, secondaryActionTex
           </Typography>
         </CardContent>
         <CardActions sx={{ flexDirection: 'column', alignItems: 'center', padding: 2 }}>
-          <Button sx={{ marginBottom: 1, textTransform: 'none' }} color="primary" onClick={onPrimaryActionClick}>
-            {primaryActionText}
-          </Button>
-          <Button variant="contained" sx={{ backgroundColor: '#052CAC', width: '100%' }} onClick={onSecondaryActionClick}>
+          <a href="https://soneri-assets.s3.me-south-1.amazonaws.com/aof/One+Pager+-+Soneri+Current+Account.pdf" target="_blank" rel="noopener noreferrer">
+            <Button sx={{
+              marginBottom: 1, textTransform: 'none',
+            }}
+              color="primary">
+              {primaryActionText}
+            </Button>
+          </a>
+          <Button variant="contained" sx={{ backgroundColor: '#052CAC', width: '100%' }} onClick={handleOpen}>
             {secondaryActionText}
           </Button>
         </CardActions>
       </Card>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="body1" sx={{ color: '#052CAC', fontWeight: 'bold' }}>
+            Thank you for showing interest in Soneri Bank digital account opening!
+          </Typography>
+          <Typography id="modal-modal-description" variant="body2" sx={{ mt: 2 }}>
+            By proceeding, I hereby confirm that I am not visually impaired/blind person, neither carry shaky signatures or a politically exposed person (PEP)/associated/related to/in close family relationship with PEP and qualified to open account digitally as per applicable laws.
+            <br />
+            Before we move forward, make sure that you have the following documents in your hand:
+            <ul>
+              <li>
+                Original NADRA identity document
+              </li>
+              <li>
+                Proof of income (job letter, salary slip, business letter)
+              </li>
+              <li>
+                Active email ID
+              </li>
+              <li>
+                Active mobile number registered or issued against your ID number
+              </li>
+              <li>
+                Camera and location enabled device
+              </li>
+            </ul>
+            Note: In case your NADRA ID is expired, please upload the original NADRA token.
+            <br />
+            If you need any assistance feel free to visit your nearest Soneri Bank branch or call 021-111-SONERI (766374)
+            <br />
+            <br />
+            <span style={{ color: '#052CAC', fontWeight: 'bold' }}>
+              Important! If you decide to proceed on web, you will need to finalize the application via our mobile app to verify your biometric details!
+            </span>
+          </Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+            <Button
+              sx={{
+                marginBottom: 1,
+                textTransform: 'none',
+                color: '#052CAC',
+                padding: '6px 35px',
+                maxWidth: 'fit-content'
+              }}
+              color="primary"
+            // onClick={onPrimaryActionClick}
+            >
+              LET'S START
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
     </Grid>
   );
 };
--- soneri-accounts-opening/src/Components/Homepage.jsx
+++ soneri-accounts-opening/src/Components/Homepage.jsx
@@ -5,71 +5,71 @@ import GenericCard from './Card';
 import cardData from './CardData';
 
 function Homepage() {
-  return (
-    <>
-      <Box
-        sx={{
-          width: '100%',
-          height: '80px',
-          background: 'linear-gradient(to bottom, #FBE198, rgba(251, 225, 152, 0))',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <img src={logo} alt="" height='60px' />
-        <Typography variant='h6' sx={{ fontWeight: 'bold' }}>
-          Soneri Digital Account Opening Portal
-        </Typography>
-        <Typography variant='body1' paddingRight='20px'>
-          Already have an open application?{' '}
-          <Button variant="contained" sx={{ backgroundColor: '#052CAC' }}>Login</Button>
-        </Typography>
-      </Box>
-      <Container maxWidth="lg" sx={{ py: 2 }}>
-        <Typography variant="body1" gutterBottom sx={{ fontWeight: 'bold', color: '#052CAC' }}>
-          Open a new account from the comfort of your home!
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          Apply and open an account online with Soneri Digital. You can browse through products in both Conventional and Mustaqeem segments below.
-        </Typography>
-        <br />
-        <Grid container spacing={2} >
-          <Grid item xs={3} sm={3} md={3}>
-            <Button variant="contained" fullWidth sx={{ backgroundColor: '#052CAC', borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }}>CONVENTIONAL</Button>
-          </Grid>
-          <Grid item xs={3} sm={3} md={3}>
-            <Button variant="outlined" sx={{ borderBottom: 'none', color: '#052CAC', borderColor: '#052CAC', borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }} fullWidth>MUSTAQEEM</Button>
-          </Grid>
-        </Grid>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#EAECFD',
-            padding: '20px 20px',
-            borderRadius: '10px'
-          }}
-        >
-          <Grid container spacing={2} justifyContent="center">
-            {cardData.map((card, index) => (
-              <GenericCard
-                key={index}
-                title={card.title}
-                description={card.description}
-                primaryActionText={card.primaryActionText}
-                secondaryActionText={card.secondaryActionText}
-                onPrimaryActionClick={card.onPrimaryActionClick}
-                onSecondaryActionClick={card.onSecondaryActionClick}
-              />
-            ))}
-          </Grid>
-        </Box>
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <Box
+                sx={{
+                    width: '100%',
+                    height: '80px',
+                    background: 'linear-gradient(to bottom, #FBE198, rgba(251, 225, 152, 0))',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                <img src={logo} alt="" height='60px' />
+                <Typography variant='h6' sx={{ fontWeight: 'bold' }}>
+                    Soneri Digital Account Opening Portal
+                </Typography>
+                <Typography variant='body1' paddingRight='20px'>
+                    Already have an open application?{' '}
+                    <Button variant="contained" sx={{ backgroundColor: '#052CAC' }}>Login</Button>
+                </Typography>
+            </Box>
+            <Container maxWidth="lg" sx={{ py: 2 }}>
+                <Typography variant="body1" gutterBottom sx={{ fontWeight: 'bold', color: '#052CAC' }}>
+                    Open a new account from the comfort of your home!
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    Apply and open an account online with Soneri Digital. You can browse through products in both Conventional and Mustaqeem segments below.
+                </Typography>
+                <br />
+                <Grid container spacing={2} >
+                    <Grid item xs={3} sm={3} md={3}>
+                        <Button variant="contained" fullWidth sx={{ backgroundColor: '#052CAC', borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }}>CONVENTIONAL</Button>
+                    </Grid>
+                    <Grid item xs={3} sm={3} md={3}>
+                        <Button variant="outlined" sx={{ borderBottom: 'none', color: '#052CAC', borderColor: '#052CAC', borderBottomLeftRadius: 0, borderBottomRightRadius: 0 }} fullWidth>MUSTAQEEM</Button>
+                    </Grid>
+                </Grid>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: '#EAECFD',
+                        padding: '20px 20px',
+                        borderRadius: '10px'
+                    }}
+                >
+                    <Grid container spacing={2} justifyContent="center">
+                        {cardData.map((card, index) => (
+                            <GenericCard
+                                key={index}
+                                title={card.title}
+                                description={card.description}
+                                primaryActionText={card.primaryActionText}
+                                secondaryActionText={card.secondaryActionText}
+                                onPrimaryActionClick={card.onPrimaryActionClick}
+                                onSecondaryActionClick={card.onSecondaryActionClick}
+                            />
+                        ))}
+                    </Grid>
+                </Box>
+            </Container>
+        </>
+    );
 }
 
 export default Homepage;
